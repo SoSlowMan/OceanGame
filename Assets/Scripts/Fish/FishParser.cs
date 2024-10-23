123ddf9b725d86f45e8512938ab96aa4594d4907
SoSlowMan <kosman.forgames@yandex.ru>
2024-10-23 21:30:38 +0300

    JSON parsing and TMPro implementation

--- Assets/Scripts/Fish/FishParser.cs
+++ Assets/Scripts/Fish/FishParser.cs
@@ -1,31 +1,40 @@
 using System.IO;
 using UnityEngine;
+using SimpleJSON;
 
 public class FishParser : MonoBehaviour
 {
-    public Fish fish;
-    readonly string fileName = "Assets/Scripts/Fish/Fish.json";
+    private JSONNode _jsonObj;
+    private string _path;
+    [SerializeField] Material material;
 
     void Start()
     {
-        ReadJSON();
-        Randomizer();
+        _path = Application.streamingAssetsPath + "/" + "Fish.json";
+        _jsonObj = JSON.Parse(File.ReadAllText(_path));
+        Debug.Log(_jsonObj[0][0][0]);
+        material.color = new Color32(_jsonObj[0][1][1], _jsonObj[0][1][2], _jsonObj[0][1][3], 255);
+        // TODO: change the list of colors in JSON to a list of materials from assets
+        //Coloring();
     }
 
-    private void ReadJSON()
-    {
-        string jsonString = File.ReadAllText(fileName);
-        fish = CreateFromJSON(jsonString);
-    }
+    //    private void ReadJSON()
+    //    {
+    //        string jsonString = File.ReadAllText(fileName);
+    //        fish = CreateFromJSON(jsonString);
+    //    }
 
-    public static Fish CreateFromJSON(string jsonString)
-    {
-        return JsonUtility.FromJson<Fish>(jsonString);
-    }
+    //    public static Fish CreateFromJSON(string jsonString)
+    //    {
+    //        return JsonUtility.FromJson<Fish>(jsonString);
+    //    }
 
-    private float[] Randomizer()
+    private void Coloring()
     {
-        float[] result = new float[4];
-        return result;
+        //switch (_jsonObj.AsArray)
+        //{
+        //    case "test1":
+        //        break;
+        //}
     }
 }
